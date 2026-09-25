f53c94ed79e00ff052dd80f21cea3cc6ad9ad2dc
George Mikhaiel <gmikhaie@ualberta.ca>
2026-09-25 14:31:39 -0600

    Update default color in Shape class from purple to red

--- app/src/main/java/com/example/coders/Shape.kt
+++ app/src/main/java/com/example/coders/Shape.kt
@@ -3,10 +3,5 @@ package com.example.coders
 abstract class Shape(
     var x: Int,
     var y: Int,
-<<<<<<< HEAD
-    val color: String = "purple"
-)
-=======
     val color: String = "red"
 )
->>>>>>> 77a8af7e87a48238c9a84a1eeede20de61a4e26a
