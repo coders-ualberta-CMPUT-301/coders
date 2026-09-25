15ef9f1b070e64e100fb07bd5c96e677a8698cf3
gmikhaie <gmikhaie@ualberta.ca>
2026-09-25 14:56:33 -0600

    Fix merge conflict in Shape.kt color property
    Resolved merge conflict in Shape class color property.

--- app/src/main/java/com/example/coders/Shape.kt
+++ app/src/main/java/com/example/coders/Shape.kt
@@ -3,14 +3,6 @@ package com.example.coders
 abstract class Shape(
     var x: Int,
     var y: Int,
-<<<<<<< HEAD
-<<<<<<<<< Temporary merge branch 1
     val color: String = "turquoise"
 )
-=========
-    val color: String = "red"
-=======
-    val color: String = "blue"
->>>>>>> david-attia
-)
->>>>>>>>> Temporary merge branch 2
+
